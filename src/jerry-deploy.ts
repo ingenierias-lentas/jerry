deec7d842fc800a227e4aba045c61b0333e00df1
Eric Solomon <esolomon1221@gmail.com>
2022-03-04 15:53:09 -0600

    Create basic subcommands using commander.js

---
+++ src/jerry-deploy.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+export {};
+
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .option('-f, --force', 'force build');
+
+program.parse(process.argv);
+
+console.log();
+console.log('-----');
+console.log('Deploying SCORE smart contract');
